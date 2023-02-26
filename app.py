628a53aeb8ecaf1ca891bbd07fcfde8676284aaa
Rolo <89317868+Roloper@users.noreply.github.com>
2023-02-26 16:12:39 -0500

    R

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
 from flask import Flask, render_template
 from config import config
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='template')
 
 @app.route('/')
 
 def index():
-    return render_template('auch/login.html')
+    return render_template('auth/login.html')
 
 if __name__ == '__main__':
     app.config.from_object(config['development'])
